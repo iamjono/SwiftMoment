3a89ee9bb61e337d24834e79e3b06cc913d84fa1
flangel <fll@scade.io>
2018-08-06 12:55:17 +0200

    Added version 4.0 tag in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // Generated automatically by Perfect Assistant Application
 // Date: 2017-10-10 14:00:01 +0000
+// swift-tools-version:4.0
 import PackageDescription
 let package = Package(
 	name: "SwiftMoment",
