c48d3c9cffeed1108d81e6649be92b15d6a81b2e
Kyle Jessup <kyle.jessup@treefrog.ca>
2019-03-26 12:02:32 -0400

    :(

--- Package.swift
+++ Package.swift
@@ -1,9 +1,14 @@
+// swift-tools-version:4.2
 // Generated automatically by Perfect Assistant Application
 // Date: 2017-10-10 14:00:01 +0000
 import PackageDescription
 let package = Package(
 	name: "SwiftMoment",
-	targets: [],
-	dependencies: [
+	products: [
+		.library(name: "SwiftMoment", targets: ["SwiftMoment"])
+	],
+	dependencies: [],
+	targets: [
+		.target(name: "SwiftMoment", dependencies: [])
 	]
 )
